f5e6246da5cd6610a0874319f8328f92315a0f84
Katona Zalán <katonazalan2453@PETOFI-ASZOD.LAN>
2025-12-12 12:35:31 +0100

    navbar

--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Products from './Products';
@@ -10,24 +10,46 @@ function NavbarComponent({ user, setUser }) {
     <BrowserRouter>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Admin Felület Demó</Navbar.Brand>
+          <Navbar.Brand as={NavLink} to='/'>Admin Felület Demó</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link as={NavLink} to='/'>Home</Nav.Link>
-              <Nav.Link as={NavLink} to='/products'>Termékek</Nav.Link>
-              <Nav.Link as={NavLink} to='/upload'>Termék feltöltése</Nav.Link>
-              <Nav.Link as={NavLink} to='/login'>Bejelentkezés</Nav.Link>
+              {
+                user ? (<Nav.Link as={NavLink} to='/products'>Termékek</Nav.Link>) : null
+              }
+
+              {
+                (user && user.rights == 1) ?
+                  (
+                    <Nav.Link as={NavLink} to='/upload'>Termék feltöltése</Nav.Link>
+                  ) : null
+              }
+              {!user ?
+                (
+                  <>
+                    <Nav.Link as={NavLink} to='/login'>Bejelentkezés</Nav.Link>
+                  </>
+                ) : null
+              }
             </Nav>
+            {user ?
+              (
+                <div className="ms-auto">
+                  <Button className="bg-danger">Kijelentkezés</Button>
+                </div>
+              ) : null
+            }
+
+
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/upload' element={<Upload />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/products' element={user ? <Products /> : <Navigate to='/login' />} />
+        <Route path='/upload' element={(user && user.rights == 1) ? <Upload /> : <Navigate to='/login' />} />
+        <Route path='/login' element={!user ? <Login /> : <Navigate to='/Home' />} />
       </Routes>
     </BrowserRouter>
   )
